2d504fef51ddcee1d1daebc41252172c47e54b08
Santhanabalan <bala2102002@gmail.com>
2023-05-13 10:23:08 +0530

    Initial Syberia Bringup

--- common.mk
+++ common.mk
@@ -422,7 +422,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-syberia
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
